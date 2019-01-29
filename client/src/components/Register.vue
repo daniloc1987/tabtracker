2497471c7c1a3eb62229469858feeaaa9b64a96b
Coppola <danilo.coppola@accenture.com>
2019-01-29 17:22:46 +0100

    'client_server'

---
+++ client/src/components/Register.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    Register<br/>
+    <input type="text" name="email" placeholder="email" v-model="email" /> <br/>
+    <input type="password" name="password" placeholder="password" v-model="password"/> <br/>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: 'abs',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
