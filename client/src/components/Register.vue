a94cf7883da2fdbf7b93bcee5ddb3d0a0102fe65
Coppola <danilo.coppola@accenture.com>
2019-02-01 17:44:09 +0100

    'Style_and_more'

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    Register<br/>
-    <input type="text" name="email" placeholder="email" v-model="email" /> <br/>
-    <input type="password" name="password" placeholder="password" v-model="password"/> <br/>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="text" name="email" placeholder="email" v-model="email" />
+          <br/>
+          <input type="password" name="password" placeholder="password" v-model="password"/>
+          <br/>
+          <div class="error" v-html="error"></div>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -13,8 +24,9 @@ export default {
   name: 'Register',
   data () {
     return {
-      email: 'abs',
-      password: '123'
+      email: '',
+      password: '',
+      error: null
     }
   },
   watch: {
@@ -24,11 +36,14 @@ export default {
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -36,18 +51,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.error{
+  color: red;
 }
 </style>
